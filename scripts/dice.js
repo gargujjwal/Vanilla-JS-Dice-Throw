69f4344cb88be3083a440b77556ccdbad1200483
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-06-04 15:31:29 +0530

    add responsiveness

--- scripts/dice.js
+++ scripts/dice.js
@@ -1,16 +1,17 @@
 function giveRandomDice() {
-    return Math.floor(Math.random() * 6) + 1;
+	return Math.floor(Math.random() * 6) + 1;
 }
 
 function changeWinner(player, draw = false) {
-    if (draw) {
-        document.querySelector(".title h1").textContent = "Draw";
-    } else {
-        document.querySelector(".title h1").textContent = "🚩 " + player + " WON";
-    }
+	if (draw) {
+		document.querySelector(".title h1").textContent = "Draw";
+	} else {
+		document.querySelector(".title h1").textContent =
+			"🚩 " + player + " WON";
+	}
 }
 
-const player1Name = prompt("Please Enter your name");
+const player1Name = prompt("Please Enter your name") || "You";
 document.querySelector("#player1").textContent = player1Name;
 
 // simulate dice roll
@@ -18,15 +19,18 @@ const player1Dice = giveRandomDice();
 const player2Dice = giveRandomDice();
 
 // change picture according to dice roll
-document.querySelector(".dice1 img").setAttribute("src", "images/dice" + player1Dice + ".png");
-document.querySelector(".dice2 img").setAttribute("src", "images/dice" + player2Dice + ".png");
-
+document
+	.querySelector(".dice1 img")
+	.setAttribute("src", "images/dice" + player1Dice + ".png");
+document
+	.querySelector(".dice2 img")
+	.setAttribute("src", "images/dice" + player2Dice + ".png");
 
 // judge the dice roll
 if (player1Dice > player2Dice) {
-    changeWinner(player1Name);
+	changeWinner(player1Name);
 } else if (player2Dice > player1Dice) {
-    changeWinner("Computer");
+	changeWinner("Computer");
 } else {
-    changeWinner(0, draw = true);
+	changeWinner(0, (draw = true));
 }
